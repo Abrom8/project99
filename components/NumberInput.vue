0469c3212295cd32241227c832b88a4945f46a3b
Abrom8 <38963270+Abrom8@users.noreply.github.com>
2022-03-13 13:48:40 +0100

    feat(inputForm): add increase/decrease buttons

---
+++ components/NumberInput.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <label class="label">
+      <span class="label-text">{{ label }}</span>
+    </label>
+    <div class="flex gap-2 flex-nowrap">
+      <button class="btn btn-square" @click="decreaseValue()">-</button>
+      <input
+        :value="currentValue"
+        type="number"
+        :step="step"
+        :placeholder="placeholder"
+        class="input input-bordered"
+      />
+      <button class="btn btn-square" @click="increaseValue()">+</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    value: {
+      type: Number,
+      default: 0,
+    },
+    step: {
+      type: Number,
+      default: 1,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    placeholder: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      currentValue: this.value,
+    }
+  },
+  watch: {
+    value(newVal) {
+      this.currentValue = newVal
+    },
+  },
+  methods: {
+    increaseValue() {
+      this.currentValue = (parseFloat(this.currentValue) + this.step).toFixed(1)
+      this.$emit('input', parseFloat(this.currentValue))
+    },
+    decreaseValue() {
+      this.currentValue = (parseFloat(this.currentValue) - this.step).toFixed(1)
+      this.$emit('input', parseFloat(this.currentValue))
+    },
+  },
+}
+</script>
+
--- components/WeightInputForm.vue
+++ components/WeightInputForm.vue
@@ -7,50 +7,29 @@
       <div class="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
         <div class="card-body">
           <div class="form-control">
-            <label class="label">
-              <span class="label-text">Weight</span>
-            </label>
-            <input
-              v-model="kg"
-              type="number"
-              step="0.1"
-              placeholder="kg"
-              class="input input-bordered"
+            <number-input
+              :value="kg"
+              :step="0.1"
+              label="Weight kg"
+              @input="kg = $event"
             />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Bodyfat %</span>
-            </label>
-            <input
-              v-model="fat"
-              type="number"
-              step="0.1"
-              placeholder="%"
-              class="input input-bordered"
+            <number-input
+              :value="fat"
+              :step="0.1"
+              label="Bodyfat %"
+              @input="fat = $event"
             />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Muscle %</span>
-            </label>
-            <input
-              v-model="muscle"
-              type="number"
-              step="0.1"
-              placeholder="%"
-              class="input input-bordered"
+            <number-input
+              :value="muscle"
+              :step="0.1"
+              label="Muscle %"
+              @input="muscle = $event"
             />
-          </div>
-          <div class="form-control">
-            <label class="label">
-              <span class="label-text">Visceral Fat</span>
-            </label>
-            <input
-              v-model="visceralFat"
-              type="number"
-              placeholder="10"
-              class="input input-bordered"
+            <number-input
+              :value="visceralFat"
+              :step="1"
+              label="Visceral Fat"
+              @input="visceralFat = $event"
             />
           </div>
 
@@ -77,11 +56,12 @@ export default {
       fat: 0,
       muscle: 0,
       visceralFat: 0,
+      test: 99,
       loading: false,
     }
   },
   async created() {
-    const { data: healthMetric} = await this.$supabase
+    const { data: healthMetric } = await this.$supabase
       .from('health_metric')
       .select('*')
       .order('date', { ascending: false })
@@ -98,17 +78,15 @@ export default {
     async submitHealthMetrics() {
       try {
         this.loading = true
-        const { error } = await this.$supabase
-          .from('health_metric')
-          .insert([
-            {
-              date: new Date(),
-              fat_percentage: this.fat,
-              muscle_percantage: this.muscle,
-              visceral_fat: this.visceralFat,
-              weight_kg: this.kg,
-            },
-          ])
+        const { error } = await this.$supabase.from('health_metric').insert([
+          {
+            date: new Date(),
+            fat_percentage: this.fat,
+            muscle_percantage: this.muscle,
+            visceral_fat: this.visceralFat,
+            weight_kg: this.kg,
+          },
+        ])
         if (error) throw error
         alert('Submitted!')
       } catch (error) {
